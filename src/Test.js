003c045e2d8937b95f141eb7231315edd9f16717
CHOIJUNGUK <9616lion@naver.com>
2022-09-11 14:45:20 +0900

    App2컴포넌트화

---
+++ src/Test.js
@@ -0,0 +1,22 @@
+import logo from './img/googlelogo_color_272x92dp.png';
+import './index.css';
+
+function App2() {
+  return(
+    <div className="App2">
+     <header class name="App2">
+      <img src={logo} className="App2-logo" alt="logo"/>
+      <p>
+      Edit <code>src/index.js</code> and save to reload.
+      </p>
+      <a
+      className="App2-link"
+       href="https://www.google.co.kr/"
+       target="_blank"
+       rel="noopener noreferrer">
+       </a>
+      </header>
+   </div>
+    )
+    }
+   export default App2;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM1 from 'react-dom/client';
 // const ReactDOM1 = requre('react-dom/client');
 import './index.css';
-import App from './App';
+import App1 from './App';
 import reportWebVitals from './reportWebVitals';
+import App2 from './Test';
+
 // 현위치 public 
 // ./ : 현위치, ../ : 상위 , 하위폴더로 이동 : /하위폴더/이미지.png
 // 현위치 : react-test/src 안에
@@ -13,23 +15,10 @@ import logo from './img/googlelogo_color_272x92dp.png';
 const root = ReactDOM1.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App1 />
+    <App2/>
     
-     <div classname="react">
-     <header class name="react-t">
-      <img src={logo} classname="react-logo" alt="logo"/>
-      <p>
-      Edit <code>src/index.js</code> and save to reload.
-      </p>
-      <a
-       className="react-link"
-       href="https://www.google.co.kr/"
-       target="_blank"
-       rel="noopener noreferrer"
-      >
-      </a>
-      </header>
-      </div>
+     
   </React.StrictMode>
 );
 
