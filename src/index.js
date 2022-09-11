7cb57a87c87ae2132908a5fd62c91f12f192b78e
CHOIJUNGUK <9616lion@naver.com>
2022-09-11 13:42:26 +0900

    google 로고 띄움

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM1 from 'react-dom/client';
+// const ReactDOM1 = requre('react-dom/client');
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// 현위치 public 
+// ./ : 현위치, ../ : 상위 , 하위폴더로 이동 : /하위폴더/이미지.png
+// 현위치 : react-test/src 안에
+// 구글로고 : react-test/public 폴더 안
+// ../react-test/public:/react-test/src/'googlelogo_color_272x92dp.png';
+import logo from './img/googlelogo_color_272x92dp.png'; 
+const root = ReactDOM1.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    
+     <div classname="react">
+     <header class name="react-t">
+      <img src={logo} classname="react-logo" alt="logo"/>
+      <p>
+      Edit <code>src/index.js</code> and save to reload.
+      </p>
+      <a
+       className="react-link"
+       href="https://www.google.co.kr/"
+       target="_blank"
+       rel="noopener noreferrer"
+      >
+      </a>
+      </header>
+      </div>
   </React.StrictMode>
 );
 
