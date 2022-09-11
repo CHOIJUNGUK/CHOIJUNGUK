6af23ce466d5ec9db9da8892c2f12944e53ad493
CHOIJUNGUK <9616lion@naver.com>
2022-09-11 17:51:24 +0900

    이벤트함수처리

---
+++ src/App3.js
@@ -0,0 +1,66 @@
+import {useState, useEffect } from 'react';
+
+function App3() {
+  // const time = new Date();
+  // const [time, setTime] = useState(new Date());
+  const [time, setTime] = useState(0);
+
+  // useEffect(() => {
+  //   // let num = time+1;
+  //   setTime(time+1);
+  // })
+
+
+  // 1증가하는 hook 방식
+  // useEffect(function () {
+  //   setTime(time+1);
+  // })
+
+//  useEffect(test1());
+
+  // useEffect =  useEffectFunc();
+
+  // useEffect(() => {
+  //   let num = time++;
+  //   setTime(num);
+    // const id = setInterval(() => {
+    //   setTime(new Date());
+    // }, 1000);
+  //   // return (() => clearInterval(id))
+
+
+  // }, []);
+
+  let button1_click = ()=> {
+  
+  
+    alert("버튼누름")
+  
+  
+  };
+
+  // function () {
+  //   console.log('버튼누름');
+  // }
+  
+  return (
+    // <div>
+    //   <h1>Clock</h1>
+    //   <span>{time.toLocaleTimeString()}</span>
+    // </div>
+    <div>
+      <button onClick={button1_click}>버튼2 </button>
+      {/* <script>
+        button1_click() => {
+          console.log("버튼누름");
+          // alert('알림');
+          // 'ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ';
+        }
+      </script> */}
+      <h1>1증가 </h1>
+      {time}
+    </div>
+  )
+}
+
+export default App3;
