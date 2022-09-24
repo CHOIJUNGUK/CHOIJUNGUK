a0304b2a8541ebc7fbc74407c60cc891d1435c3b
CHOIJUNGUK <9616lion@naver.com>
2022-09-24 16:40:22 +0900

    axios사용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+
+  let fname = '';
+  axios({
+    method:"get",
+    url:"https://www.melivecode.com/api/users/1",
+    responseType:"type"
+})
+.then(function(response){
+  console.log('----------------------');
+  // console.log(response);
+  // console.log(response.data);
+  console.log(response.data.user.fname);
+  fname = response.data.user.fname;
+  console.log(response.data.user.lname);
+  console.log(response.data.user.avatar);
+})
+
+  
+
+  
+  
+  
+  
+//   function searchApi() {
+//     const url = "https://jsonplaceholder.typicode.com/photos";
+//     axios.get(url)
+//     .then(function(response) {
+//         setPhotos(response.data);
+//         console.log("성공");
+//     })
+//     .catch(function(error) {
+//         console.log("실패");
+//     })
+// }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code></code> and save to reload. {fname}
         </p>
         <a
           className="App-link"
@@ -20,6 +54,6 @@ function App() {
       </header>
     </div>
   );
-}
+  }
 
 export default App;
